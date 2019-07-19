69f717514a88a5180fb42c6359b9101c877e79d9
Iliya-hacker <45923437+Iliya-hacker@users.noreply.github.com>
2019-07-19 23:15:43 +0300

    Add files via upload

---
+++ Ball Project-2/Ball Project-2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Ball_Project_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Moon = new System.Windows.Forms.PictureBox();
+            this.GetBall = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Moon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            // 
+            // Moon
+            // 
+            this.Moon.Image = global::Ball_Project_2.Properties.Resources.kisspng_earth_supermoon_lunar_phase_moon_png_no_background_5ab035c9dc6088_6390594215214975459027;
+            this.Moon.Location = new System.Drawing.Point(286, 281);
+            this.Moon.Name = "Moon";
+            this.Moon.Size = new System.Drawing.Size(158, 166);
+            this.Moon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Moon.TabIndex = 1;
+            this.Moon.TabStop = false;
+     
+            // 
+            // GetBall
+            // 
+            this.GetBall.Location = new System.Drawing.Point(12, 12);
+            this.GetBall.Name = "GetBall";
+            this.GetBall.Size = new System.Drawing.Size(245, 84);
+            this.GetBall.TabIndex = 1;
+            this.GetBall.Text = "Make Moon Faster";
+            this.GetBall.UseVisualStyleBackColor = true;
+            this.GetBall.Click += new System.EventHandler(this.Run_Moon_Faster);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 84);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ClearAll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1310, 512);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GetBall);
+            this.Controls.Add(this.Moon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Moon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Moon;
+        private System.Windows.Forms.Button GetBall;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ball Project-2/Ball Project-2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ball_Project_2
+{
+    public partial class Form1 : Form
+    {
+        int dx = 1;
+        int dy = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Moon.Visible = false;
+        }
+
+
+
+        private void Run_Moon_Faster(object sender, EventArgs e)
+        {
+            Action run = () => { Moon.Location = new Point(Moon.Location.X + dx, Moon.Location.Y + dy); };
+            Moon.Visible = true;
+
+            //updating coordinates
+            
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        if (Moon.Location.X + dx <= 0 || Moon.Location.X + dx >= ClientSize.Width - Moon.Width)
+                        {
+                            dx = -dx;
+                            Boing();
+                        }
+
+                        if (Moon.Location.Y + dy <= 0 || Moon.Location.Y + dy >= ClientSize.Height - Moon.Height)
+                        {
+                            dy = -dy;
+                            Boing();
+                        }
+
+
+                        Moon.BeginInvoke(run);
+                        Thread.Sleep(10);
+
+                    }
+                    
+                });
+           
+            
+    
+        }
+ 
+        private void Boing()
+        {
+            using (SoundPlayer player = new SoundPlayer(Properties.Resources.chord))
+            {
+                if (Moon.Visible)
+                {
+                    player.Play();
+                }
+
+                else
+                    player.Stop();
+            }
+        }
+
+        private void ClearAll(object sender, EventArgs e)
+        {
+            Moon.Visible = false;
+        }
+
+    }
+}
